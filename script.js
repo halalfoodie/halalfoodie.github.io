d4b02f7fe7ee3647dfce448d1379601e80b047e7
Emran Emon <30546312+emranemon@users.noreply.github.com>
2023-10-21 16:58:34 +0900

    refactoring: 2-step auth example

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ $(document).ready(function() {
   });
 });
 
+var button = document.getElementById('expandButton');
+var content = document.getElementById('expandableContent');
+
+button.addEventListener('click', function () {
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        button.textContent = 'Read Less >>';
+    } else {
+        content.style.display = 'none';
+        button.textContent = 'Read More >>';
+    }
+});
+
