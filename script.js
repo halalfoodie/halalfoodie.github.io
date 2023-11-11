60bca4da56458e7f11f28180fe7b96651f40eb50
Emran Emon <30546312+emranemon@users.noreply.github.com>
2023-11-11 18:58:00 +0900

    Japanese Translation Added

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var content = document.getElementById('expandableContent');
 button.addEventListener('click', function () {
     if (content.style.display === 'none' || content.style.display === '') {
         content.style.display = 'block';
-        button.textContent = 'Read Less >>';
+        button.textContent = '<< Minimize 最小化';
     } else {
         content.style.display = 'none';
-        button.textContent = 'Read More >>';
+        button.textContent = 'Expand 拡大 >>';
     }
 });
 
